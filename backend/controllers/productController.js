b578ad938ff6ad673869a8df553dc85b41ef6256
optanwar <optanwar10@gmail.com>
2025-06-07 03:44:57 +0530

    create reaview

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -96,42 +96,6 @@ exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
 }  ) 
 
 // Create new review or update the review
-// exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
-//   const { rating, comment, productId } = req.body;
-
-//   const review = {
-//     user: req.user._id,
-//     name: req.user.name,
-//     rating: Number(rating),
-//     comment
-//   };
-//   const product = await Product.findById(productId);
-
-//   const isReviewed = product.reviews.find(
-//     (rev) => rev.user.toString() === req.user._id.toString()
-//   ); 
-
-//   if (isReviewed) {
-//     product.reviews.forEach((rev) => {
-//       if (rev.user.toString() === req.user._id.toString()) {
-//         (rev.rating = rating), (rev.comment = comment);
-//       }
-//     }
-//     );
-//   }
-//   else {
-//     product.reviews.push(review);
-//     product.numOfReviews = product.reviews.length;
-//   }
-
-//   product.ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
-//   await product.save({ validateBeforeSave: false });
-//   res.status(200).json({
-//     success: true,
-//     message: 'Review submitted successfully!'
-//   });
-// }
-// );
 
 
 exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
