7d6e7d44fee27a5b99bf7863901d14f0de304215
optanwar <optanwar10@gmail.com>
2025-06-10 14:44:04 +0530

    update reset url password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,7 @@ exports.forgotPassword = catchAsyncErrors(async (req, res ,next) => {
 
     // Create reset password url
 
-    const resetPasswordUrl = `https:://optanwar-ecommerce.vercel.app/api/v1/password/reset/${resetToken}`;
+    const resetPasswordUrl = `https:://optanwar-ecommerce.vercel.app/password/reset/${resetToken}`;
     
 
     const message = `Your password reset token is as follow:\n\n ${resetPasswordUrl} \n\nIf you have not requested this email, then ignore it. `;
